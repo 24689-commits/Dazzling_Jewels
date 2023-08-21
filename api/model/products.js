fc887a8e66f66e12b210fde69d2235807d77c619
siyamanga22 <zwenisiyamanga@gmail.com>
2023-08-21 09:17:16 +0200

    done

--- api/model/products.js
+++ api/model/products.js
@@ -1,1 +1,78 @@
-// you
+const db =require("../config")
+
+class Products{
+    fetchProducts(req,res){
+        const query =`
+        SELECT prodID, prodName, amount, category, image,gender
+        FROM Products;
+        `
+        db.query(query,(err,results)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    }
+    fetchProduct(req,res){
+        const query =`
+        SELECT prodID, prodName, amount, category, image,gender
+        FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        db.query(query, (err, result) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                result
+            });
+        }); 
+    }
+    addProduct(req, res) {
+        const query = `
+        INSERT INTO Products
+        SET ?;
+        `;
+
+        db.query(query, [req.body], (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: "The product was added."
+            });
+        });
+    }
+
+    updateProduct(req, res) {
+        const query = `
+        UPDATE Products
+        SET ?
+        WHERE prodID = ?;
+        `;
+
+        db.query(query, [req.body, req.params.id], (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: "The product record was updated."
+            });
+        });
+    }
+
+    deleteProduct(req, res) {
+        const query = `
+        DELETE FROM Products
+        WHERE prodID = ${req.params.id};
+        `;
+
+        db.query(query, (err) => {
+            if (err) throw err;
+            res.json({
+                status: res.statusCode,
+                msg: "A product record was deleted."
+            });
+        });
+    }
+}
+
+module.exports = Products;
--- api/model/users.js
+++ api/model/users.js
@@ -1,1 +1,135 @@
-// you
+const db = require("../config")
+const{hash,compare,hashSync}=require('bcrypt')
+const {createToken} = require('../middleware/authenticateUser')
+class Users{
+    fetchUsers(req,res){
+        const query =`
+        SELECT userID, firstName, lastName, gender, userDOB, emailAdd, userPassword, profileUrl
+        FROM Users;
+        `
+        db.query(query,(err,results)=>{
+            if (err) res.json({err})
+            res.json({
+                status:res.statusCode,
+                results
+            })
+        })
+    }
+    fetchUser(req,res){
+        const query = `
+        SELECT userID, firstName, lastName, gender, userDOB, emailAdd, userPassword, profileUrl
+        FROM Users
+        WHERE userID =${req.params.id};
+        `
+        db.query(query,(err,result)=>{
+            if (err) throw err
+            res.json({
+                status:res.statusCode,
+                result
+            })
+        })
+    }
+    login(req, res) {
+        const {emailAdd, userPassword} = req.body
+        
+        const query = `
+        SELECT userID, firstName, lastName, gender, userDOB, emailAdd, userPassword, profileUrl
+        FROM Users
+        WHERE emailAdd = '${emailAdd}';
+        `
+        db.query(query, async (err, result)=>{
+            if(err) throw err
+            if(!result?.length){
+                res.json({
+                    status: res.statusCode, 
+                    msg: "You provided a wrong email."
+                })
+            }else {
+                await compare(userPassword,
+                    result[0].userPassword,
+                    (cErr, cResult)=>{
+                        if(cErr) throw cErr
+                        
+                        const token =
+                        createToken({
+                            emailAdd,
+                            userPassword
+                        })
+                        if(cResult) {
+                            res.json({
+                                msg: "Logged in",
+                                token,
+                                result: result[0]
+                            })
+                        }else {
+                            res.json({
+                                status: res.statusCode,
+                                msg:
+                                "Invalid password or you have not registered"
+                            })
+                        }
+                    })
+            }
+        })
+}
+async register(req,res){
+    const data = req.body
+ 
+    data.userPassword = await hash(data.userPassword,15)
+
+    const user = {
+        emailAdd : data.emailAdd,
+        userPassword : data.userPassword
+    }
+
+    const query =`
+    INSERT INTO Users
+    SET ?;
+    `
+    db.query(query,[data], (err)=>{
+    if(err)throw err
+    let token = createToken(user)
+  
+    res.json({
+        status : res.statusCode,
+        token,
+        msg: "you are now registered."
+    })
+})
+}
+updateUser(req,res){
+    const data =req.body
+    if(data.userPassword){
+        data.userPassword = hashSync(data.userPassword,15)
+    }
+    const query =`
+    UPDATE Users
+    SET ?
+    WHERE userID=?
+    `
+    db.query(query, [data, req.params.id],(err)=>{
+       if (err) throw err
+       res.json({
+        status: statusCode,
+        msg: "The user record was updated."
+
+       })
+ 
+    })
+}
+deleteUser(req,res){
+    const query = `
+    DELETE FROM Users
+    WHERE userID =${req.params.id}
+    `
+    db.query(query,(err)=>{
+        if (err) throw err
+        res.json({
+            status:res.statusCode,
+            msg: "A user record was deleted."
+        })
+    })
+}
+
+}
+module.exports = Users
