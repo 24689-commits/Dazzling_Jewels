93a6bb66ad189f377fb75fb9ff05bf01b58c08d1
siyamanga <zwenisiyamanga@gmail.com>
2023-08-23 11:45:45 +0200

    done

---
+++ djeomp_gui/src/components/ProductModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product-modal" v-if="showModal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2>{{ selectedProduct.prodName }}</h2>
+      <p>Category: {{ selectedProduct.category }}</p>
+      <p>Price: R{{ selectedProduct.amount }}</p>
+      <!-- Add more product details here if needed -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectedProduct: Object, // The product to display in the modal
+    showModal: Boolean, // Controls whether the modal is shown or hidden
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close"); // Emit an event to close the modal
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Your modal styling goes here */
+</style>
--- djeomp_gui/src/views/ProductsView.vue
+++ djeomp_gui/src/views/ProductsView.vue
@@ -35,9 +35,33 @@
             <div class="col">
               <img :src="Product.image" class="card-img-top img fluid" :alt="Product.prodName" />
               <div class="card-body">
-                <h5 class="card-title">{{ Product.prodName }}</h5>
-                <p class="card-text">Category: {{ Product.category }}</p>
-                <h5 class="card-title"> R{{ Product.amount }}</h5>
+                <h6 class="card-title">{{ Product.prodName }}</h6>
+                <h6 class="card-text">Category: {{ Product.category }}</h6>
+                <h6 class="card-title"> R{{ Product.amount }}</h6>
+                <button class="view-button" @click="showPopover(Product)" >View More</button>
+              </div>
+            </div>
+            <div v-if="Product.showPopover" class="modal-background">
+              <button class="close-button" @click="closeModal(Product)">Close</button>
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="card mb-3 " style="max-width: 700px; margin: 0 auto">
+                    <div class="row g-0 justify-content-center align-items-center">
+                      <div class="col-md-4 text-center">
+                        <img :src="Product.image" class="img-fluid" style="width:100rem!important; height:15rem;" :alt="Product.prodName" />
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          <h3 class="card-title">{{ Product.prodName }}</h3>
+                          <p class="card-text">Category: {{ Product.category }}</p>
+                          <p class="card-title">R{{ Product.amount }}</p>
+                          <p><u>Description : </u></p>
+                          <p class="card-text">{{ Product.about }}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -98,6 +122,15 @@ export default {
       selectedCategory: '',
       selectedGender: '',
       sortingOption: 'default',
+      product:{
+        showPopover:false,
+        image: '', 
+        prodName: '',
+        category: '',
+        amount: '',
+        about: '',
+      }
+     
     };
   },
   methods: {
@@ -118,6 +151,13 @@ export default {
     applySorting() {
 
     },
+   showPopover(product) {
+      product.showPopover = true;
+    },
+    closeModal(product) {
+  product.showPopover = false;
+},
+
   },
   mounted() {
     this.$store.dispatch('fetchProducts');
@@ -151,7 +191,6 @@ export default {
   border: 1px solid #ccc;
   border-radius: 5px;
 }
-
 .filters {
   margin-top: 20px;
 }
@@ -188,4 +227,57 @@ li:hover{
 .row{
   margin-left: 8rem;
 }
-</style>
+.modal-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10000; 
+}
+
+.modal-dialog {
+  position: absolute; 
+  top: 30%; 
+  left:30%; 
+  height:8
+  00px;
+  width:800px;
+  z-index: 10001
+}
+.view-button{
+  border: 1px solid black;
+  border-radius: 10px;
+  width:100px;
+  height:40px;
+  cursor: pointer;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: 1px solid black;
+  border-radius: 10px;
+  width:100px;
+  height:50px;
+  cursor: pointer;
+  z-index: 10002;
+  background-color:#d2d2d2
+}
+.card-body {
+  padding: 20px;
+  text-align: center; 
+}
+
+.card-title {
+  font-size: 24px; /* Increase font size for the title */
+}
+
+.card-text {
+  font-size: 18px; /* Increase font size for text content */
+}</style>
